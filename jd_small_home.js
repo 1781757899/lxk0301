7e02b235a44a4ba3f16118a65e0cb79d6f46caee
liuning0307 <72370295+liuning0307@users.noreply.github.com>
2020-12-17 09:31:12 +0800

    Update jd_small_home.js

--- jd_small_home.js
+++ jd_small_home.js
@@ -136,7 +136,7 @@ async function doChannelsListTask(taskId, taskType) {
 async function helpFriends() {
   await updateInviteCode();
   if (!$.inviteCodes) await updateInviteCodeCDN();
-  if (!$.inviteCodes) await updateInviteCodeCDN('https://raw.githubusercontent.com/liuning0307/try/main/jd_small_home_invitecodes.json');
+  if (!$.inviteCodes) await updateInviteCodeCDN('https://raw.githubusercontent.com/liuning0307/lxk0301/main/jd_small_home_invitecodes.json');
   for (let item of $.inviteCodes.inviteCode) {
     if (!item) continue
     await createAssistUser(item, $.createAssistUserID || "1318106976846299138");
@@ -766,7 +766,7 @@ function login(userName) {
     })
   })
 }
-function updateInviteCode(url = 'https://raw.githubusercontent.com/liuning0307/try/main/jd_small_home_invitecodes.json') {
+function updateInviteCode(url = 'https://raw.githubusercontent.com/liuning0307/lxk0301/main/jd_small_home_invitecodes.json') {
   return new Promise(resolve => {
     $.get({url}, async (err, resp, data) => {
       try {
@@ -783,7 +783,7 @@ function updateInviteCode(url = 'https://raw.githubusercontent.com/liuning0307/t
     })
   })
 }
-function updateInviteCodeCDN(url = 'https://raw.fastgit.org/liuning0307/try/main/jd_small_home_invitecodes.json') {
+function updateInviteCodeCDN(url = 'https://raw.fastgit.org/liuning0307/lxk0301/main/jd_small_home_invitecodes.json') {
   return new Promise(async resolve => {
     $.get({url}, async (err, resp, data) => {
       try {
